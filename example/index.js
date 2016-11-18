2c5062b00fa4cbd0a1fa5a1d803131c0f6796cec
James Barwell <jb@jamesbarwell.co.uk>
2016-11-18 13:49:36 +0000

    Move example app port

--- example/index.js
+++ example/index.js
@@ -5,7 +5,7 @@ base.setName('example')
 
 base.config.set({
     // Port to listen on
-    HTTP_PORT: { type: 'int', default: 3000 },
+    HTTP_PORT: { type: 'int', default: 1337 },
     // Name of person to greet
     GREETING_NAME: { type: 'string', default: 'world' }
 })
