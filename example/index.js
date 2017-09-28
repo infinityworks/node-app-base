adf93fe5b07e7b331365d99476ef1da6f5e106d3
Luke Pearson <luke.pearson@infinityworks.com>
2017-09-28 10:15:59 +0100

    Fixed example syntax error

--- example/index.js
+++ example/index.js
@@ -5,7 +5,10 @@ base.config.set({
     // Port to listen on
     HTTP_PORT: { type: 'int', default: 1337 },
     // Name of person to greet
-    GREETING_NAME: { type: 'string', default: 'world' }
+    GREETING_NAME: { type: 'string', default: 'world' },
+    SLACK_ENABLED: { type: 'bool', default: false },
+    SLACK_URL: { type: 'string', default: 'insert_slack_webhook_here' },
+    SLACK_CHANNEL: { type: 'string', default: '#testchannel' }
 })
 
 const app = require('./app')
